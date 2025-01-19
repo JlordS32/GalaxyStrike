a6c428286687d025b0019c88c434c6a722f3ad7b
JlordS32 <jlordsty128@gmail.com>
2025-01-19 12:29:39 +1100

    update

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -3,12 +3,12 @@ using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
-    [SerializeField] TMP_Text scoreboardText;
+    [SerializeField] TMP_Text _scoreboardText;
 
     private int _score = 0;
 
     public void IncreaseScore(int amount) {
         _score += amount;
-        scoreboardText.text = $"Score: {_score}";
+        _scoreboardText.text = $"Score: {_score}";
     }
 }   
